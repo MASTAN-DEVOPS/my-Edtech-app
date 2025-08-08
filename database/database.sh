f436ce893d6a26a4c8d69797391fcfef5e4dd33d
MDMASTAN222 <mdmastan0205@gmail.com>
2025-08-08 23:39:25 +0530

    database code

---
+++ database/database.sh
@@ -0,0 +1,6 @@
+#// server/config/db.js
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
+
