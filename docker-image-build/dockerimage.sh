bf2a89c9dd6cd33d137cc384b73dd9bc2c2125d0
MDMASTAN222 <mdmastan0205@gmail.com>
2025-08-08 23:58:49 +0530

    docker image build

---
+++ docker-image-build/dockerimage.sh
@@ -0,0 +1,8 @@
+# Backend
+docker build -t mdmastan222/genius-backend ./server
+docker push mdmastan222/genius-backend
+
+# Frontend
+docker build -t mdmastan222/genius-frontend ./client
+docker push mdmastan222/genius-frontend
+
